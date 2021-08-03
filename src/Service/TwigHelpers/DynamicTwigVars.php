16839398d4bc69e9fb89c29c6921e8a1b4453942
lozitskiys <lozitskiys@gmail.com>
2021-08-03 16:15:37 +0300

    Create DynamicTwigVars.php

---
+++ src/Service/TwigHelpers/DynamicTwigVars.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Verse\Service\TwigHelpers;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * Container to pass global variables to twig.
+ *
+ * @author Stas Lozitskiy
+ */
+class DynamicTwigVars implements ContainerInterface
+{
+    private array $vars;
+
+    public function set(string $id, mixed $value)
+    {
+        $this->vars[$id] = $value;
+    }
+
+    public function get(string $id)
+    {
+        return $this->vars[$id];
+    }
+
+    public function has(string $id)
+    {
+        return isset($this->vars[$id]);
+    }
+}
